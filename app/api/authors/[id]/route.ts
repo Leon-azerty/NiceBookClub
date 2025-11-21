44e406c3f700f80f22f4023e5da0e7aee5c02d13
Leon-azerty <maxime.noel@epitech.eu>
2025-11-21 08:02:33 +0100

    :sparkles: add route to get, update and delete an author

---
+++ app/api/authors/[id]/route.ts
@@ -0,0 +1,61 @@
+// app/api/books/[id]/route.ts
+
+import { authorSchema } from '@/app/common/schema';
+import { prisma } from '@/lib/prisma';
+import { NextResponse } from 'next/server';
+
+export async function GET(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+
+  const author = await prisma.author.findUnique({
+    where: { id },
+    include: { books: true },
+  });
+  if (author) {
+    return NextResponse.json(author);
+  } else {
+    return NextResponse.json({ error: 'Author not found' }, { status: 404 });
+  }
+}
+
+// Il faut aussi qu'on puisse modifier les livres de l'auteur ?
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = await params;
+  try {
+    const body = await req.json();
+    const parsedAuthor = authorSchema.parse(body);
+
+    const updatedAuthor = await prisma.author.update({
+      where: { id },
+      data: {
+        name: parsedAuthor.name,
+      },
+    });
+    return NextResponse.json(updatedAuthor);
+  } catch (error) {
+    console.error('Error updating author:', error);
+    return Response.json({ error: 'error' }, { status: 500 });
+  }
+}
+
+export async function DELETE(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  const { id } = params;
+  try {
+    const deletedAuthor = await prisma.author.delete({
+      where: { id },
+    });
+    return NextResponse.json(deletedAuthor);
+  } catch (error) {
+    console.error('Error deleting author:', error);
+    return Response.json({ error: 'error' }, { status: 500 });
+  }
+}
